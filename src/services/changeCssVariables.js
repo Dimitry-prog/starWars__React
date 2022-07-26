adfbc0065893811b8492508c7a83c42ab17f1cde
Dimitry-prog <kfifa3@gmail.com>
2022-07-26 16:54:10 +0300

    change for pages

--- src/services/changeCssVariables.js
+++
@@ -1,7 +0,0 @@
-export const changeCssVariables = theme => {
-  const root = document.querySelector(':root');
-  const cssVariables = ['color', 'bgImage'];
-  cssVariables.forEach(element => {
-    root.style.setProperty(`--theme-default-${element}`, `var(--theme-${theme}-${element}`);
-  });
-}
--- src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import favoriteReducer from '../reducers/favoriteReducer';
-
-export default combineReducers({
-  favoriteReducer,
-});
